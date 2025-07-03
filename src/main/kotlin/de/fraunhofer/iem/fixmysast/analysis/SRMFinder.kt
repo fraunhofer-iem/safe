fd17809534c3f8fd7cc1dc4d5ef11acb2d9404db
rohith <srk240998@gmail.com>
2025-07-03 14:05:04 +0200

    Rename SRMLoader

--- src/main/kotlin/de/fraunhofer/iem/fixmysast/analysis/SRMFinder.kt
+++ src/main/kotlin/de/fraunhofer/iem/fixmysast/analysis/SRMFinder.kt
@@ -1,6 +1,6 @@
 package de.fraunhofer.iem.fixmysast.analysis
 
-import de.fraunhofer.iem.fixmysast.data.SrmLoader
+import de.fraunhofer.iem.fixmysast.data.SRMLoader
 
 /***
  * Finds SRMs in the target application.
@@ -9,22 +9,22 @@ import de.fraunhofer.iem.fixmysast.data.SrmLoader
 object SRMFinder {
 
     fun isSRM(signature: String): Boolean {
-        val method = SrmLoader.srmCatalogMethods[signature]
+        val method = SRMLoader.srmCatalogMethods[signature]
         return method != null && (method.srm.isNotEmpty() || method.cwe.isNotEmpty())
     }
 
     fun getSrmCategory(signature: String): List<String> {
-        val method = SrmLoader.srmCatalogMethods[signature]
+        val method = SRMLoader.srmCatalogMethods[signature]
         return method?.srm ?: emptyList()
     }
 
     fun getCweCategory(signature: String): List<String> {
-        val method = SrmLoader.srmCatalogMethods[signature]
+        val method = SRMLoader.srmCatalogMethods[signature]
         return method?.cwe ?: emptyList()
     }
 
     fun getSrmAndCweCategory(signature: String): List<String> {
-        val method = SrmLoader.srmCatalogMethods[signature]
+        val method = SRMLoader.srmCatalogMethods[signature]
         return if (method != null) {
             method.srm + method.cwe
         } else {
