438da7386c968cfd6a9bbade7adb719fab9146fd
ranjithkris <ranjith.masthikatte@gmail.com>
2025-07-06 13:48:32 +0200

    Taking the projects root directory dynamically to get the SARIF file. This is to avoid 'The program works in my machine conflict'

--- src/main/kotlin/de/fraunhofer/iem/fixmysast/actions/FixMySASTAction.kt
+++ src/main/kotlin/de/fraunhofer/iem/fixmysast/actions/FixMySASTAction.kt
@@ -6,15 +6,18 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
 import de.fraunhofer.iem.fixmysast.comm.ParseFileNotifier
 import de.fraunhofer.iem.fixmysast.sast.SASTParser
+import java.io.File
 
 class FixMySASTAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+        //TODO: This file name must be taken dynamically and not hardcoded.
+        val SARIF_FILE = "Benchmark_1.2-Semgrep-v1.123.0_Edited.sarif"
 
         //Save file location
         PropertiesComponent.getInstance(e.project!!)
             .setValue("de.fraunhofer.iem.fixmysast.actions.SastFile",
-                "/home/oshando/IdeaProjects/BenchmarkJava/results/Benchmark_1.2-Semgrep-v1.123.0_Editedx.sarif")
+                e.project!!.basePath + File.separator + "results" + File.separator + SARIF_FILE)
 
 
         if (PropertiesComponent.getInstance(e.project!!)
@@ -24,7 +27,7 @@ class FixMySASTAction : AnAction() {
                 e.project!!,
                 PropertiesComponent.getInstance(e.project!!)
                     .getValue("de.fraunhofer.iem.fixmysast.actions.SastFile",
-                        "/BenchmarkJava/results/Benchmark_1.2-Semgrep-v1.123.0_Editedx.sarif")
+                        e.project!!.basePath + File.separator + "results" + File.separator + SARIF_FILE)
             )
         } else {
 
