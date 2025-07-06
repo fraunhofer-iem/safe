b38f3491c08d8821f302097975616d1cfa80c638
ranjithkris <ranjith.masthikatte@gmail.com>
2025-07-06 14:35:56 +0200

    Refactoring: Moving all the LLM related to llmService package with modularity. Moving the models into sast/dataModel/models

--- src/main/kotlin/de/fraunhofer/iem/fixmysast/sast/ExpertiseLevel.kt
+++
@@ -1,9 +0,0 @@
-package de.fraunhofer.iem.fixmysast.sast
-
-enum class ExpertiseLevel(val label: String) {
-    BEGINNER("Beginner"),
-    INTERMEDIATE("Intermediate"),
-    ADVANCED("Advanced");
-
-    override fun toString() = label
-}
