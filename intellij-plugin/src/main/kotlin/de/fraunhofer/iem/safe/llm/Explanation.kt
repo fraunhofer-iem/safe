6a75518acaf288fb2a7d7b1dc7a80892aba2ae1d
osh78988 <oshando.johnson@iem.fraunhofer.de>
2026-04-23 16:27:34 +0200

    Remove old plugin tool window implementation

--- intellij-plugin/src/main/kotlin/de/fraunhofer/iem/safe/llm/Explanation.kt
+++
@@ -1,8 +0,0 @@
-package de.fraunhofer.iem.safe.llm
-
-data class Explanation(
-    val overview: String,
-    val explanation: String,
-    val exampleCode: String,
-    val exampleCodeExplanation: String
-)
